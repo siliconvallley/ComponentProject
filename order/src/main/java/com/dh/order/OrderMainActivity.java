a1043a104331324dacc67fbacfd5ed2428c7bff3
Silicon_Valley <siliconvalley.dh@foxmail.com>
2020-09-17 02:00:49 +0800

    搭建组件化基础架构

---
+++ order/src/main/java/com/dh/order/OrderMainActivity.java
@@ -0,0 +1,14 @@
+package com.dh.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity_main);
+    }
+}
